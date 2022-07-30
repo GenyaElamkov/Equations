fa086aba1fec3ce1186668cc410921e7718f86d7
GenyaElamkov <GenyaElamkov@gmail.com>
2022-07-30 09:55:24 +0300

    Generate equal v1.1 С корректировал функции

--- main.py
+++ main.py
@@ -7,58 +7,45 @@ from time import sleep
 """
 
 
+def generate_equate(size, factor, summa):
+    max_size = summa  # Максимальный размер множителя на "х"
+    sign = ('+', '-', '*',)
+    summa = randint(1, summa)
+
+    while True:
+        # Генерируем уравнения.
+        line = ''
+        for _ in range(size):
+            equate = f'{randint(1, factor)}*x {choice(sign)} '
+            line += equate
+
+        line = line[:-2].rstrip()
+
+        # Проверка на равенство уравнения.
+        for x in range(1, max_size):
+            # x - используется в eval() для расчета
+            if int(eval(line)) == summa:
+                res = f'{line} = {summa};x = {x}'
+                return res
+
+
 def progress_bar(counter):
     for _ in tqdm(range(counter), ncols=80, ascii=True, desc='Total'):
         sleep(0.1)
 
 
-def generate_equate(size, factor, summa, number_equations):
-    """
-    Генерирует линейное уравнение.
-    :param factor:
-    :param size: int.
-    :param summa: int.
-    :param number_equations: int.
-    :return: list.
-    """
-    max_size = summa  # Максимальный размер множителя на "х"
-    sign = ('+', '-', '*',)
-    resoult = []
-    counter = 0
-    while counter < number_equations:
-        summa = randint(1, summa)
-        flag = False
-        line = ''
-        while not flag:
-            # Генерируем уравнения.
-            for _ in range(size):
-                equate = f'{randint(1, factor)}*x {choice(sign)} '
-                line += equate
-
-            line = line[:-2].rstrip()
-
-            # Проверка на равенство уравнения.
-            for x in range(1, max_size):
-                # x - используется в eval() для расчета
-                if int(eval(line)) == summa:
-                    res = f'{line} = {summa};x = {x}'
-                    resoult.append(res.split(';'))
-                    flag = True
-                    break
-
-            if flag:
-                counter += 1
-                progress_bar(counter)
-                break
-            else:
-                line = ''
-
-    return resoult
+def count_equate(number_equations, size, factor, summa,):
+    arr = []
+    for i in range(1, number_equations + 1):
+        arr.append(generate_equate(size, factor, summa).split(';'))
+        progress_bar(i)
+    return arr
 
 
 def replace_chars(arr):
     """
     Меняет символ в уравнении на привычный нам вид.
+    [!] Пока не используется.
     :param arr: list
     :return: list
     """
@@ -82,8 +69,9 @@ def main():
     factor = int(input('[3] Введите множитель (10, 100, 1000): '))  # factor*X = summa.
     summa = int(input('[2] Введите max сумму равенства: '))  # Равенство ур-я = 100.
     number_equations = int(input('[4] Введите кол-во уравнений: '))  # Кол-во ур-й.
-    arr = generate_equate(size, factor, summa, number_equations)
+    arr = count_equate(number_equations, size, factor, summa)
     print_equate(replace_chars(arr))
+    input('[!] Ваш файл создан, находится рядом с программой')
 
 
 if __name__ == '__main__':
