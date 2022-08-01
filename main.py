d0719800eb1784ce8933cbf7dde1e48dd4487a77
GenyaElamkov <GenyaElamkov@gmail.com>
2022-08-01 10:15:54 +0300

    Generate equal v1.2 Insert generate bracket

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import randint, choice
+from random import randint, choice, randrange
 from tqdm import tqdm
 from time import sleep
 
@@ -7,7 +7,28 @@ from time import sleep
 """
 
 
-def generate_equate(size, factor, summa):
+def generate_bracket(text) -> str:
+    """Генератор скобок"""
+    arr = text.split()
+
+    start = randrange(0, len(arr) - 1, 2)
+    end = randrange(start, len(arr) - 1, 2)
+    if start == end and start == 0:
+        end = randrange(start + 2, len(arr), 2)
+    elif start == end and end == len(arr) - 1:
+        start = randrange(0, end - 1, 2)
+    elif start == end and start >= 1:
+        start = randrange(0, start + 1, 2)
+        end = randrange(start + 2, len(arr), 2)
+
+    arr[start] = '(' + arr[start]
+    arr[end] = arr[end] + ')'
+
+    return ' '.join(arr)
+
+
+def generate_equate(size, factor, summa) -> str:
+    """Возвращает сгенерированная уравнение."""
     max_size = summa  # Максимальный размер множителя на "х"
     sign = ('+', '-', '*',)
     summa = randint(1, summa)
@@ -21,9 +42,12 @@ def generate_equate(size, factor, summa):
 
         line = line[:-2].rstrip()
 
+        # Генератор скобок.
+        line = generate_bracket(line)
+
         # Проверка на равенство уравнения.
         for x in range(1, max_size):
-            # x - используется в eval() для расчета
+            # x - используется в eval() для расчета.
             if int(eval(line)) == summa:
                 res = f'{line} = {summa};x = {x}'
                 return res
@@ -34,23 +58,12 @@ def progress_bar(counter):
         sleep(0.1)
 
 
-def count_equate(number_equations, size, factor, summa,):
+def count_equate(number_equations, size, factor, summa, ) -> list:
+    """Возвращает кол-во уравнений."""
     arr = []
     for i in range(1, number_equations + 1):
         arr.append(generate_equate(size, factor, summa).split(';'))
         progress_bar(i)
-    return arr
-
-
-def replace_chars(arr):
-    """
-    Меняет символ в уравнении на привычный нам вид.
-    [!] Пока не используется.
-    :param arr: list
-    :return: list
-    """
-    for line in arr:
-        line[0] = ''.join(line[0]).replace('/', ':')
     return arr
 
 
@@ -64,15 +77,20 @@ def print_equate(arr):
             f.writelines(f'{i + 1}) {arr[i][1]} ')
 
 
+def test_generate_equate():
+    print(generate_equate(size=5, factor=3, summa=100))
+
+
 def main():
     size = int(input('[1] Введите кол-во аргументов: '))  # Размер ур-я аргументов.
     factor = int(input('[3] Введите множитель (10, 100, 1000): '))  # factor*X = summa.
     summa = int(input('[2] Введите max сумму равенства: '))  # Равенство ур-я = 100.
     number_equations = int(input('[4] Введите кол-во уравнений: '))  # Кол-во ур-й.
     arr = count_equate(number_equations, size, factor, summa)
-    print_equate(replace_chars(arr))
+    print_equate(arr)
     input('[!] Ваш файл создан, находится рядом с программой')
 
 
 if __name__ == '__main__':
     main()
+    # test_generate_equate()
